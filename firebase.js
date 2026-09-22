fe72be44fee3bfacbb32e63be12dd7571ba07d2f
nagylilbidaa <naglylilbidaa@gmail.com>
2026-09-22 20:34:39 +0000

    Add Firebase configuration to firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjmj1TjIjWrJjhisP-Or8OFYJ3TCZRSxs",
+  authDomain: "nagli-6f321.firebaseapp.com",
+  projectId: "nagli-6f321",
+  storageBucket: "nagli-6f321.firebasestorage.app",
+  messagingSenderId: "19948171002",
+  appId: "1:19948171002:web:568a05fbdaba240879a343",
+  measurementId: "G-D96CXV6Z01"
+};
