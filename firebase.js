c6bf01e8bdeb59a31e7483b678527d9a9468f247
nagylilbidaa <naglylilbidaa@gmail.com>
2026-09-22 21:42:14 +0000

    Initialize Firebase with configuration settings

--- firebase.js
+++ firebase.js
@@ -8,3 +8,25 @@ const firebaseConfig = {
   appId: "1:19948171002:web:568a05fbdaba240879a343",
   measurementId: "G-D96CXV6Z01"
 };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5Wxnuy0CwhKSiSArMoW1r3VRwEb2jLqY",
+  authDomain: "nagly-81892.firebaseapp.com",
+  databaseURL: "https://nagly-81892-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "nagly-81892",
+  storageBucket: "nagly-81892.firebasestorage.app",
+  messagingSenderId: "868776490039",
+  appId: "1:868776490039:web:f5ee42a18804acc68bbbb2",
+  measurementId: "G-9GZEQ0PFBH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
