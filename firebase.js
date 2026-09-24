4cd568c93c35500b4a8bf6be84429549bafe9a95
nagylilbidaa <naglylilbidaa@gmail.com>
2026-09-24 09:59:48 +0000

    Replace Firebase config with new project settings
    Updated Firebase configuration with new project details.

--- firebase.js
+++ firebase.js
@@ -1,17 +1,20 @@
   
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIza...",
-  authDomain: "naqli-xxxxx.firebaseapp.com",
-  projectId: "naqli-xxxxx",
-  storageBucket: "naqli-xxxxx.appspot.com",
-  messagingSenderId: "123456",
-  appId: "1:123456:web:abc"
-};
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js"
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5Wxnuy0CwhKSiSArMoW1r3VRwEb2jLqY",
+  authDomain: "nagly-81892.firebaseapp.com",
+  databaseURL: "https://nagly-81892-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "nagly-81892",
+  storageBucket: "nagly-81892.firebasestorage.app",
+  messagingSenderId: "868776490039",
+  appId: "1:868776490039:web:f5ee42a18804acc68bbbb2",
+  measurementId: "G-9GZEQ0PFBH"
+};
 
 
